28f4583301ea5542c1f7822a313f9673c6862c55
r56wq <cdnudjundun@gmail.com>
2024-02-18 09:18:22 +0800

    hw1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,11 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
+import java.nio.Buffer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +22,10 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        first = 0;
+        last = 0;
+        this.capacity = capacity;
+        rb = (T[])new Object[capacity];
     }
 
     /**
@@ -30,6 +35,12 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("The buff is full");
+        }
+        rb[last] = x;
+        fillCount = fillCount + 1;
+        last = (last + 1 + capacity) % capacity;
     }
 
     /**
@@ -38,7 +49,14 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if(isEmpty()) {
+            throw new RuntimeException("The buff is empty");
+        }
+        T item = rb[first];
+        first = (first + 1 + capacity) % capacity;
+        fillCount--;
+        return item;
     }
 
     /**
@@ -46,7 +64,24 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayListIterator();
+    }
+
+    public class ArrayListIterator<T> implements Iterator<T> {
+        @Override
+        public boolean hasNext() {
+          return !isEmpty();
+        }
+
+        @Override
+        public T next() {
+            return (T) dequeue();
+        }
+    }
 }
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,9 +9,24 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
+        arb.enqueue(1);
+        arb.enqueue(4);
+        arb.enqueue(10);
+        assertEquals(arb.dequeue(), (Integer) 1);
+        assertEquals(arb.dequeue(), (Integer) 4);
     }
 
+    @Test
+    public void testIterator() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
+        arb.enqueue(1);
+        arb.enqueue(4);
+        arb.enqueue(10);
+        for(int i : arb) {
+            System.out.println(i);
+        }
+    }
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
