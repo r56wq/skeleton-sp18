dcf58d45e533666b18e3a42c0351a02f5776d55e
r56wq <cdnudjundun@gmail.com>
2024-03-17 10:55:34 +0800

    mod

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,15 +6,22 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private final WeightedQuickUnionUF sites;
     private final boolean[] opens; //true if open, false if not open
-    private boolean[] fulls; //true if is full, false if not
+    private int openNums;
+    private final int virtualTop;
+    private final int virtualBottom;
     private final int length;
+
     public Percolation(int N) {
         if (N < 0 || N == 0) {
             throw new java.lang.IllegalArgumentException("No a valid argument");
         } else {
-            sites = new WeightedQuickUnionUF(N * N);
-            opens = new boolean[N];
+            sites = new WeightedQuickUnionUF(N * N + 2); /*2 is left for virtual top and bottom
+            index N*N  is the virtual bottom and N*N + 1 is the virtual top*/
+            opens = new boolean[N * N];
             length = N;
+            virtualTop = N * N + 1;
+            virtualBottom = N * N;
+            openNums = 0;
         }
     }
 
@@ -32,25 +39,25 @@ public class Percolation {
         }
 
         //look around and see if other sites are full
-        if (checkValid(row, col - 1)) {
+        if (checkValid(row, col - 1) && isOpen(row, col - 1)) {
             int idx1 = xyT1D(row, col);
             int idx2 = xyT1D(row, col - 1);
             sites.union(idx1, idx2);
         }
 
-        if (checkValid(row, col + 1)) {
+        if (checkValid(row, col + 1) && isOpen(row, col + 1)) {
             int idx1 = xyT1D(row, col);
             int idx2 = xyT1D(row, col + 1);
             sites.union(idx1, idx2);
         }
 
-        if (checkValid(row - 1, col)) {
+        if (checkValid(row - 1, col) && isOpen(row - 1, col)) {
             int idx1 = xyT1D(row, col);
             int idx2 = xyT1D(row - 1, col);
             sites.union(idx1, idx2);
         }
 
-        if (checkValid(row + 1, col)) {
+        if (checkValid(row + 1, col) && isOpen(row + 1, col)) {
             int idx1 = xyT1D(row, col);
             int idx2 = xyT1D(row + 1, col);
             sites.union(idx1, idx2);
@@ -61,7 +68,7 @@ public class Percolation {
     * helper method to check if a point is a valid point
      */
     private boolean checkValid(int row, int col) {
-        return (xyT1D(row, col) < ((length * length) - 1));
+        return (xyT1D(row, col) < (length * length)) && (xyT1D(row, col) > 0 || xyT1D(row, col) == 0) ;
     }
 
     /*
@@ -72,9 +79,18 @@ public class Percolation {
             throw new java.lang.IndexOutOfBoundsException("Index" + row + " " + col
                     + "is out of boundary");
         }
+
+        openNums++;
         int idx = xyT1D(row, col);
         opens[idx] = true;
+        if (row == 0) { /* connect the first row to the virtual top */
+            sites.union(idx, virtualTop);
+        }
+        if (row == length - 1) { /* connect the last row to the virtual bottom */
+            sites.union(idx, virtualBottom);
+        }
         checkNearby(sites, row, col);
+
     }
 
     /*
@@ -96,7 +112,49 @@ public class Percolation {
             throw new java.lang.IndexOutOfBoundsException("Index" + row + " " + col
                     + "is out of boundary");
         }
-        return (fulls[xyT1D(row, col)]);
+
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        int idx1 = xyT1D(row, col);
+        int idx2; //index of another site
+        //to avoid back wash
+        if (row == length - 1) {
+            if (checkValid(row, col - 1) && isOpen(row, col - 1)) {
+                idx2 = xyT1D(row, col - 1);
+                if (sites.connected(idx2, virtualTop)) {
+                    return true;
+                }
+            }
+
+            if (checkValid(row, col + 1) && isOpen(row, col + 1)) {
+                idx2 = xyT1D(row, col + 1);
+                if (sites.connected(idx2, virtualTop)) {
+                    return true;
+                }
+            }
+
+            if (checkValid(row - 1, col) && isOpen(row - 1, col)) {
+                idx2 = xyT1D(row - 1, col);
+                if (sites.connected(idx2, virtualTop)) {
+                    return true;
+                }
+            }
+
+
+            return false; // if no full site nearby
+        }
+        return sites.connected(xyT1D(row, col), virtualTop);
     }
 
+    /*
+    * return number of open sites
+     */
+    public int numberOfOpenSites() {
+        return openNums;
+    }
+
+    public boolean percolates() {
+        return sites.connected(virtualTop, virtualBottom);
+    }
 }
