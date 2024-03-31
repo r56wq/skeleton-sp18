46fb336cc12064de4d3440df389ec56bb0d4a7ae
r56wq <cdnudjundun@gmail.com>
2024-04-01 00:08:31 +0800

    done

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,6 @@
 package lab11.graphs;
-
+import java.util.ArrayDeque;
+import java.util.Queue;
 /**
  *  @author Josh Hug
  */
@@ -9,21 +10,65 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> queue = new ArrayDeque<>();
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        int v = s;
+        marked[v] = true;
+        distTo[v] = 0;
+        queue.add(v);
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+        while (!queue.isEmpty()) {
+            v = queue.poll();
+            for (int w : maze.adj(v)) {
+                if (w == t) {
+                    marked[w] = true;
+                    distTo[w] = distTo[v] + 1;
+                    edgeTo[w] = v;
+                    return; //target found
+                }
+
+                if (!marked[w]) {
+                    marked[w] = true;
+                    queue.add(w);
+                    distTo[w] = distTo[v] + 1;
+                    edgeTo[w] = v;
+                    announce();
+                }
+            }
+        }
+
     }
+
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,4 +1,6 @@
 package lab11.graphs;
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.Stack;
 
 /**
  *  @author Josh Hug
@@ -9,7 +11,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private static Stack<Integer> stack = new Stack<>();
+    private static ArrayList<Integer> prevPoints;
     public MazeCycles(Maze m) {
         super(m);
     }
@@ -17,8 +20,56 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        int s = 0;
+        prevPoints = new ArrayList<>(maze.V());
+        for (int i = 0; i < maze.V(); i++) {
+            prevPoints.add(0);
+        }
+        prevPoints.set(s, 0); //the previous point of the start is itself
+        stack.push(s);
+        dfs(s);
+
     }
 
     // Helper methods go here
+    private void dfs(int s) {
+        marked[s] = true;
+        while (!stack.isEmpty()) {
+          int v = stack.pop();
+          for (int w : maze.adj(v)) {
+              if (!marked[w]) {
+                  marked[w] = true;
+                  prevPoints.set(w, v);
+                  stack.push(w);
+                  edgeTo[w] = v;
+                  announce();
+              } else {
+                  //if it is marked
+                  if (cycleDetect(v, w)) { //v is the current node and w is the next node
+                      return;
+                  }
+              }
+          }
+      }
+    }
+
+    private boolean cycleDetect(int v, int w) {
+        // invariant
+        int prev = v;
+        int current = w;
+        if (prevPoints.get(current) != prev) {
+            //cycle exist
+            edgeTo[current] = prev;
+            current = prev;
+            while ((prev = prevPoints.get(prev)) != w) {
+                edgeTo[current] = prev;
+                current = prev;
+            }
+            edgeTo[current] = prev;
+            announce();
+            return true;
+        }
+        return false;
+    }
 }
 
